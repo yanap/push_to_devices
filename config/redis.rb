8166d8ded7876fbdd4789d77e6b4b90f4d1f3337
tabito ohtani <koko1000ban@gmail.com>
2013-02-19 12:46:06 +0900

    Fixed config path

--- config/redis.rb
+++ config/redis.rb
@@ -1,5 +1,5 @@
 if %w(staging production).include? PADRINO_ENV || ENV["USE_REDIS_FAILOVER"]
-  redis_failover_config = YAML.load_file(Padrino.root + 'config/redis_failover.yml')[PADRINO_ENV]
+  redis_failover_config = YAML.load_file(Padrino.root('config', 'redis_failover.yml'))[PADRINO_ENV]
   $redis = RedisFailover::Client.new(redis_failover_config.symbolize_keys) do |client|
     client.on_node_change do |master, slaves|
       Padrino::logger.info("Nodes changed! master: #{master}, slaves: #{slaves}")
